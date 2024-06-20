0e9d1a1712d0b568b6a271fe58406bc219f5a6f7
dikidwihermawan <dikidwihermawan@gmail.com>
2024-06-20 07:05:42 +0700

    add controler, model, routes mongodb server mongoose & api

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://dikidwihermawan:cEJdAac73gNX2ewc@cluster0.uzu9uoq.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(4000, () => console.log("Connection Success"));
+  })
+  .catch((err) => console.log(err));
