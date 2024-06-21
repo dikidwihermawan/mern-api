cb0ed9ad2ae6c6efde76a28815a3571558467a96
dikidwihermawan <dikidwihermawan@gmail.com>
2024-06-21 07:39:51 +0700

    add crud blog

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -4,7 +4,6 @@ const { body } = require("express-validator");
 const router = express.Router();
 
 const blogController = require("../controllers/blog");
-
 router.post(
   "/post",
   [
@@ -13,5 +12,17 @@ router.post(
   ],
   blogController.createBlogPost
 );
+router.get("/posts", blogController.getAllBlogPost);
+router.get("/post/:postId", blogController.getBlogPostById);
+router.put(
+  "/post/:postId",
+  [
+    body("title").isLength({ min: 5 }).withMessage("Input Title tidak sesuai"),
+    body("body").isLength({ min: 5 }).withMessage("Input Body tidak sesuai"),
+  ],
+  blogController.updateBlogPost
+);
+
+router.delete("/post/:postId", blogController.deleteBlogPost);
 
 module.exports = router;
